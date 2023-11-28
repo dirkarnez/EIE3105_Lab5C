b250cff744cc6a6c381d53c8a325deed783b4cf0
dirkarnez <smalldirkalex@gmail.com>
2023-11-29 00:27:11 +0800

    - upload files

--- main.c
+++ main.c
@@ -1,23 +1,48 @@
 #include "stm32f10x.h"                  // Device header
 
-void delay(int t);
+void DelayMs(uint32_t ms);
+void SysTick_Handler(void);
 
-void delay(int t) {
-	int i;
-	for(i = 0; i < t; i++)
-		GPIOA->BSRR |= 0x01; // do something to PA0
+static __IO uint32_t msTicks;
+
+void DelayMs(uint32_t ms)
+{
+	// Reload us value
+	msTicks = ms;
+	// Wait until usTick reach zero
+	while (msTicks);
+}
+
+// SysTick_Handler function will be called every 1 ms
+void SysTick_Handler(void)
+{
+	if (msTicks != 0)
+	{
+		msTicks--;
+	}
 }
 
 int main(void) {
-	//GPIO set up for PA5 (on-board LED)
-	RCC->APB2ENR |= RCC_APB2Periph_GPIOA; //Enable APB2 peripheral clock
-	GPIOA->CRL &= (unsigned int)~0x00F00000; //clear the setting
-	GPIOA->CRL |= 0 << 22 | 2 << 20; //GPIO_Mode_Out_PP, GPIO_Speed_2MHz
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
+	
+	//Enable APB2 periphral clock
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	// Update SystemCoreClock value
+	SystemCoreClockUpdate();
+	
+	// Configure the SysTick timer to overflow every 1 ms
+	SysTick_Config(SystemCoreClock / 1000);
 	
 	while(1) {
-		GPIOA->BSRR |= 0x20;
-		delay(3000000);
-		GPIOA->BRR  |= 0x20;
-		delay(3000000);
-	}
+		GPIO_WriteBit(GPIOA, GPIO_Pin_5, Bit_SET);
+		DelayMs(1000);
+		GPIO_WriteBit(GPIOA, GPIO_Pin_5, Bit_RESET);
+		DelayMs(1000);
+	}	
 }
